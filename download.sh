2e4688afa7d428434253dd9172869b85c0085998
Pavel Sviderski <pavel@psviderski.name>
2018-03-16 18:41:03 +0300

    speed up download script, skip mirroring if sitemap wasn't updated

--- download.sh
+++ download.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-set -e
+set -ex
 DOMAIN=welcome.stepik.org
 
-wget -q https://$DOMAIN/sitemap.xml --no-cache -O - | egrep -o "https://$DOMAIN[^<]+" | wget -i - --timestamping --page-requisites --span-hosts --domains tildacdn.com,tilda.ws --adjust-extension
+wget --timestamping --no-cache --force-directories https://$DOMAIN/sitemap.xml
+# If sitemap.xml was updated run wget to mirror all pages listed in it
+if [[ ! -z $(git status -s) ]]; then
+    wget -q https://$DOMAIN/sitemap.xml --no-cache -O - | egrep -o "https://$DOMAIN[^<]+" | \
+    wget -i - --timestamping --page-requisites --span-hosts --domains tildacdn.com,tilda.ws --adjust-extension
+fi
