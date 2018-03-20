7098bb11228ee52e2008427c1a3a0426a13d8b59
Pavel Sviderski <pavel@psviderski.name>
2018-03-20 17:55:23 +0300

    add bioinf.me to domains list to backup

--- download.sh
+++ download.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -ex
-DOMAIN=welcome.stepik.org
+DOMAINS=(
+    welcome.stepik.org
+    bioinf.me
+)
 
-wget --timestamping --no-cache --force-directories https://$DOMAIN/sitemap.xml
-# If sitemap.xml was updated run wget to mirror all pages listed in it
-if [[ ! -z $(git status -s) ]]; then
-    wget -q https://$DOMAIN/sitemap.xml --no-cache -O - | egrep -o "https://$DOMAIN[^<]+" | \
-    wget -i - --timestamping --page-requisites --span-hosts --domains tildacdn.com,tilda.ws --adjust-extension
-fi
+for domain in ${DOMAINS[@]}; do
+    wget --timestamping --no-cache --force-directories https://$domain/sitemap.xml
+    # If sitemap.xml was updated run wget to mirror all pages listed in it
+    if [[ ! -z $(git status -s) ]]; then
+        wget -q https://$domain/sitemap.xml --no-cache -O - | egrep -o "https://$domain[^<]+" | \
+        wget -i - --timestamping --page-requisites --span-hosts --domains tildacdn.com,tilda.ws --adjust-extension
+    fi
+done
